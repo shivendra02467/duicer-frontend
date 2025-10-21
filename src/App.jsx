e0edf203838119571dea523c315206d0634c53ce
Shivendra Sharma <shivendra02467@gmail.com>
2025-10-21 18:54:58 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame } from "@react-three/fiber";
 import { motion } from "framer-motion";
 
 
@@ -26,7 +26,7 @@ function Home() {
   return (
     <motion.div className="flex min-h-screen items-center justify-center text-center relative overflow-hidden" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       <Canvas
-        style={{ width: '40vw', height: '40vh'}}
+        style={{ width: "40vw", height: "40vh"}}
         gl={{ alpha: true, antialias: true }}
         camera={{ position: [0, 0, 3], fov: 75 }}
       >
@@ -35,13 +35,13 @@ function Home() {
         <Icosahedron />
       </Canvas>
       <div className="absolute w-[35vh] h-[35vh] top-0 bottom-0 left-0 right-0 m-auto animate-[spin_20s_linear_infinite]">
-        <img src='/dodeca-1.svg' className='absolute bottom-0 right-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]'/>
-        <img src='/dodeca-2.svg' className='absolute top-0 right-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]'/>
-        <img src='/icosa-1.svg' className='absolute top-0 left-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]'/>
-        <img src='/icosa-2.svg' className='absolute bottom-0 left-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]'/>
+        <img src="/dodeca-1.svg" className="absolute bottom-0 right-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]"/>
+        <img src="/dodeca-2.svg" className="absolute top-0 right-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]"/>
+        <img src="/icosa-1.svg" className="absolute top-0 left-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]"/>
+        <img src="/icosa-2.svg" className="absolute bottom-0 left-0 w-[25%] h-[25%] animate-[spin_5000ms_linear_infinite]"/>
       </div>
       <h2 className="text-4xl font-bold absolute top-[10vh]">Exploring Math, Physics, Programming and Fun Ideas</h2>
-      <img src='/duicer.svg' className='absolute m-auto top-0 right-0 bottom-0 left-0 w-[36vh] h-[20vh] [filter:drop-shadow(0.8vh_0.5vh_0px_rgba(0,0,0,1))]'/>
+      <img src="/duicer.svg" className="absolute m-auto top-0 right-0 bottom-0 left-0 w-[36vh] h-[20vh] [filter:drop-shadow(0.8vh_0.5vh_0px_rgba(0,0,0,1))]"/>
       <div className="flex absolute bottom-[10vh] gap-10 justify-center items-center">
         <a
           href="https://www.youtube.com/@duicer"
@@ -49,7 +49,7 @@ function Home() {
           rel="noopener noreferrer"
           aria-label="Go to YouTube"
         >
-          <img src='/youtube.svg' />
+          <img src="/youtube.svg" />
         </a>
         <a
           href="https://github.com/shivendra02467"
@@ -57,7 +57,7 @@ function Home() {
           rel="noopener noreferrer"
           aria-label="Go to Github"
         >
-          <img src='/github.svg' />
+          <img src="/github.svg" />
         </a>
       </div>
     </motion.div>
